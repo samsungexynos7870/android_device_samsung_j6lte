c0c3798f956993de9cb79717837035ff3e69abd9
Alejandro <apamarin@gmail.com>
2020-06-11 08:36:02 +0200

    j6lte: set S20 fingerprint for all variants

--- aosp_j6lte.mk
+++ aosp_j6lte.mk
@@ -29,15 +29,16 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 PRODUCT_DEVICE := j6lte
 PRODUCT_NAME := aosp_j6lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-J600F
+PRODUCT_MODEL := SM-G981B
 PRODUCT_MANUFACTURER := samsung
 
 TARGET_VENDOR := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-TARGET_BOOT_ANIMATION_RES := 1920
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 
 # Build info
-BUILD_FINGERPRINT :="samsung/j6ltexc/j6lte:9.0.0/R16NW/J730FXXU6CSK6:user/release-keys"
+BUILD_FINGERPRINT :="samsung/x1sxxx/x1s:10/QP1A.190711.020/G981BXXU1ATCT:user/release-keys"
+
