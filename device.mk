05cbc38cd3e118354399f2833dd660736ced7d7a
Alejandro <apamarin@gmail.com>
2020-06-16 13:35:20 +0200

    j6lte: rework BT and build custom 7870 hild

--- device.mk
+++ device.mk
@@ -37,8 +37,12 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-impl.7870 \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/bluetooth/bt_vendor.conf:vendor/etc/bluetooth/bt_vendor.conf
 
 # Fingerprint
 PRODUCT_PACKAGES += \
