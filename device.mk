8887e9944d39c6fc64f96669c08b6110bd047453
Alejandro <apamarin@gmail.com>
2020-06-18 12:20:52 +0200

    j6lte: revert "Rework BT and build custom 7870 hild"
    This reverts commit 9d9c847ffbf5ba53cc9da40c3d1c5fb5356c2355.

--- device.mk
+++ device.mk
@@ -37,12 +37,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl.7870 \
-    android.hardware.bluetooth@1.0-service \
-    libbt-vendor
-
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/bluetooth/bt_vendor.conf:vendor/etc/bluetooth/bt_vendor.conf
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \
