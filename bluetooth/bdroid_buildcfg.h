3bf9f770eeb4f29ffd7fc52c26d24ceeb31d37a6
Alejandro <apamarin@gmail.com>
2020-06-22 07:48:03 +0200

    j6lte: revert "Rework BT and build custom 7870 hild"
    This reverts commit 1b6c77a69a7ac1719cdfbc116e2d9cdf5ba17e99.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,14 +18,13 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy J6 2018"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy J7 2017"
 
-#define BTM_WBS_INCLUDED        TRUE    /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED   FALSE   /* Don't use WBS by default */
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
 
-#define BLE_VND_INCLUDED        TRUE
+#define BLE_VND_INCLUDED TRUE
 
-/* Disable the eSCO commands */
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #endif
