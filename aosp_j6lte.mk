545cac996292b9bcecd4e5a327b33510f49ae2a4
Alejandro <apamarin@gmail.com>
2020-07-06 00:18:47 +0200

    j6lte: AEX changes

--- aosp_j6lte.mk
+++ aosp_j6lte.mk
@@ -22,8 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from j6lte device
 $(call inherit-product, device/samsung/j6lte/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j6lte
