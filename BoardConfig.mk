3eae73475d404c6a3fb22e86bcd58ecf3e79d5e5
Brian Gotte <bngotte@gmail.com>
2021-03-14 17:45:20 -0300

    Add "j6ltedx" to "TARGET_OTA_ASSERT_DEVICE"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := device/samsung/j6lte
 BOARD_USE_TFA_AMP := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := j6lte,j6ltecis,j6ltexx,j6lteub,j6lteins,j6ltedtvvj,j6ltekx
+TARGET_OTA_ASSERT_DEVICE := j6lte,j6ltecis,j6ltexx,j6lteub,j6lteins,j6ltedtvvj,j6ltekx,j6ltedx
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
